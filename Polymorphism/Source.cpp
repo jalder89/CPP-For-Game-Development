f62f2114d6a1d8ebac32e97099f414f4299b8a9d
Jessica Alder <jessalder1989@gmail.com>
2024-02-13 11:22:31 -0800

    Updating to include practice with dynamic casting to call unique functions within elements of a polymorphic array.

--- Polymorphism/Source.cpp
+++ Polymorphism/Source.cpp
@@ -5,18 +5,33 @@ class Object
 {
 public:
 	virtual void BeginPlay();
+
+	void ObjectFunction()
+	{
+		cout << "ObjectFunction() was called\n\n";
+	}
 };
 
 class Actor : public Object
 {
 public:
 	virtual void BeginPlay() override;
+
+	void ActorFunction()
+	{
+		cout << "ActorFunction() was called\n\n";
+	}
 };
 
 class Pawn : public Actor
 {
 public:
 	virtual void BeginPlay() override;
+
+	void PawnFunction()
+	{
+		cout << "PawnFunction() was called\n\n";
+	}
 };
 
 int main()
@@ -28,7 +43,27 @@ int main()
 	Object* ObjectArray[] = { objectPtr, actorPtr, pawnPtr };
 	for (int i = 0; i < 3; i++)
 	{
+		// Pymorphism
 		ObjectArray[i]->BeginPlay();
+
+		// Casting
+		Object* obj = ObjectArray[i];
+		Actor* act = dynamic_cast<Actor*>(obj);
+		Pawn* pawn = dynamic_cast<Pawn*>(obj);
+
+		if (act && pawn == NULL)
+		{
+			act->ActorFunction();
+		} 
+		else if (pawn)
+		{
+			pawn->PawnFunction();
+		}
+		else
+		{
+			obj->ObjectFunction();
+		}
+
 	}
 
 	delete objectPtr;
@@ -39,15 +74,15 @@ int main()
 
 void Object::BeginPlay()
 {
-	cout << "Object BeginPlay() called\n" << endl;
+	cout << "Object BeginPlay() called" << endl;
 }
 
 void Actor::BeginPlay()
 {
-	cout << "Actor BeginPlay() called\n" << endl;
+	cout << "Actor BeginPlay() called" << endl;
 }
 
 void Pawn::BeginPlay()
 {
-	cout << "Pawn BeginPlay() called\n" << endl;
+	cout << "Pawn BeginPlay() called" << endl;
 }
